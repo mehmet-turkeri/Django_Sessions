b9bca18d9a80ff9d7882dc975bc3fe8778ab0f33
mehmet-turkeri <mehmetturkeri51@gmail.com>
2022-10-14 15:20:59 +0200

    up

---
+++ 021_FlightAppAndToolsSetup/main/settings/prod.py
@@ -0,0 +1,27 @@
+from .base import *
+# on Live:
+DEBUG = False
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+]
+
+INSTALLED_APPS += [
+    
+]
+
+MIDDLEWARE += [
+    
+]
+
+# PostreSQL:
+DATABASES = { 
+    "default": { 
+        "ENGINE": "django.db.backends.postgresql_psycopg2", 
+        "NAME": config("POSTGRESQL_DATABASE"), 
+        "USER": config("POSTGRESQL_USER"), 
+        "PASSWORD": config("POSTGRESQL_PASSWORD"), 
+        "HOST": config("POSTGRESQL_HOST"), 
+        "PORT": config("POSTGRESQL_PORT"), 
+        "ATOMIC_REQUESTS": True, 
+    }
+}
